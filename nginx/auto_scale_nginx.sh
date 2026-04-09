22d55d57142f691e22c667bed17e690ff515f024
Nicolas Zdrobau Tamas <nicolas.zdrobau@estudiants.urv.cat>
2026-04-09 11:10:38 +0200

    afegit autoscaling

--- nginx/auto_scale_nginx.sh
+++ nginx/auto_scale_nginx.sh
@@ -6,7 +6,8 @@ API2="10.0.1.103:5000"
 LOG_FILE="/var/log/nginx/access.log"
 STATE_FILE="/tmp/nginx_autoscale_state"
 
-THRESHOLD=50
+UP_THRESHOLD=50
+DOWN_THRESHOLD=10
 INTERVAL=5
 
 if [ ! -f "$STATE_FILE" ]; then
@@ -19,14 +20,15 @@ while true; do
   sleep $INTERVAL
 
   CURRENT_COUNT=$(wc -l < "$LOG_FILE")
-  DELTA=$((CURRENT_COUNT - LAST_COUNT))
+  DELTA=$((CURRENT_COUNT - LAST_COUNT)) # Number of requests 
   LAST_COUNT=$CURRENT_COUNT
 
   STATE=$(cat "$STATE_FILE")
 
-  if [ "$STATE" = "single" ] && [ "$DELTA" -gt "$THRESHOLD" ]; then
-    echo "High load detected: $DELTA requests in $INTERVAL seconds"
-    echo "Adding second API to NGINX upstream..."
+  # SCALE UP
+  if [ "$STATE" = "single" ] && [ "$DELTA" -gt "$UP_THRESHOLD" ]; then  #si numero de requests es mayor a 50, se escala
+    echo "High load detected: $DELTA requests"
+    echo "Scaling UP..."
 
     sudo sed -i "/server $API1;/a \        server $API2;" "$NGINX_CONF"
     sudo nginx -t && sudo systemctl reload nginx
@@ -34,4 +36,17 @@ while true; do
     echo "double" > "$STATE_FILE"
     echo "Scale-up complete."
   fi
+
+  # SCALE DOWN
+  if [ "$STATE" = "double" ] && [ "$DELTA" -lt "$DOWN_THRESHOLD" ]; then #si numero de requests es menor a 10, se escala hacia abajo
+    echo "Low load detected: $DELTA requests"
+    echo "Scaling DOWN..."
+
+    sudo sed -i "/server $API2;/d" "$NGINX_CONF"
+    sudo nginx -t && sudo systemctl reload nginx
+
+    echo "single" > "$STATE_FILE"
+    echo "Scale-down complete."
+  fi
+
 done
